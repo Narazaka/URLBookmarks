1d12f72bdafe538b81bcde49ca3be70384fb7130
Narazaka <info@narazaka.net>
2024-12-20 05:23:49 +0900

    fix

---
+++ Udon/SendToUrlBookmark.cs
@@ -0,0 +1,36 @@
+﻿using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace Narazaka.VRChat.UrlBookmarks
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class SendToUrlBookmark : UdonSharpBehaviour
+    {
+        UrlBookmark UrlBookmark;
+
+        void Start()
+        {
+            var components = GetComponentsInChildren<UrlBookmark>();
+            foreach (var component in components)
+            {
+                if (Networking.IsOwner(component.gameObject))
+                {
+                    UrlBookmark = component;
+                    break;
+                }
+            }
+        }
+
+        public void OnUrlChange()
+        {
+            UrlBookmark.OnUrlChange();
+        }
+
+        public void OnUseUrl()
+        {
+            UrlBookmark.OnUseUrl();
+        }
+    }
+}
